c79febe5389dc50b67391d99d639972a78e9fcd6
d1azdn <diazsaputramc@gmail.com>
2024-05-16 21:41:24 +0700

    added navbar-prototype

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,13 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+//ini taruh di template<HelloWorld msg="Vite + Vue" />
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-    <p class="text-xs">kontol</p>
+  <div class="mx-12 mt-6">
+    <Navbar/>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
